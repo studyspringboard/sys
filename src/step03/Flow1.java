6dc13dcfc0c1e19d7ee2e56ce1dcfce3cf8c1b04
dudtl <dudtl@192.168.200.104>
2020-10-29 21:30:34 +0900

    조건문

---
+++ src/step03/Flow1.java
@@ -0,0 +1,33 @@
+package step03;
+
+import java.util.Scanner;
+
+public class Flow1 {
+	public static void main(String[] args) {
+		int month = 0;
+		
+		System.out.print("현재 월을 입력하시오.>");
+		
+		Scanner scanner = new Scanner(System.in);
+		String tmp = scanner.nextLine();
+		month = Integer.parseInt(tmp);
+		
+		switch (month) {
+		case 3:
+		case 4:
+		case 5:
+			System.out.println("현재 계절은 봄입니다.");
+			break;
+		case 6: case 7: case 8:
+			System.out.println("현재 계절은 여름입니다.");
+			break;
+		case 9: case 10: case 11:
+			System.out.println("현재 계절은 여름입니다.");
+			break;
+		default:
+		case 12: case 1: case 2:
+			System.out.println("현재 계절은 겨울입니다.");
+			
+		}
+	}
+}
