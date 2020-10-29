07f885fc124998bb806af78bd2c6aea3ac68d693
dudtl <dudtl@192.168.200.104>
2020-10-29 21:31:05 +0900

    반복문

---
+++ src/step04/FlowEx4.java
@@ -0,0 +1,21 @@
+package step04;
+
+public class FlowEx4 {
+	public static void main(String[] args) {
+//		for (int i = 1; i <= 5; i++) {
+//			for (int j = 1; j <= 5; j++) {
+//				System.out.printf("[%d, %d]",i ,j);
+//			}
+//			System.out.println();
+//		}
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				if(i==j)
+					System.out.printf("[%d, %d]",i ,j);
+				else
+					System.out.printf("%5c",' ');
+			}
+			System.out.println();
+		}
+	}
+}
